56b78060537f05b415956f3e86f07df78d275840
RajeshHaridoss <92581320+RajeshHaridoss@users.noreply.github.com>
2022-03-16 07:32:30 +0000

    Update Jsfunction.js
    updated the function with correct code

--- CodeChallenge3/Jsfunction.js
+++ CodeChallenge3/Jsfunction.js
@@ -1,15 +1,14 @@
-function findvalue(object, key, defval) {
-    if (typeof defval == "undefined") defval = null;
-    key = key.split("/");
-    for (var i = 0; i < key.length; i++) {
-      if (typeof object[key[i]] == "undefined") return defval;
-      object = object[key[i]];
-    }
-    return object;
+function findvalue(object, key) {
+  key = key.split("/");
+  for (var i = 0; i < key.length; i++) {
+    if (typeof object[key[i]] == "undefined") return null;
+    object = object[key[i]];
   }
-  
-  //var object = {"a":{"b":{"c":"d"}}};
-  //var key = 'a/b/c' ;
-  var object = { x: { y: { z: "a" } } };
-  var key = "x/y/z";
-  console.log("The value of " + key + " is " + findvalue(object, key));
+  return object;
+}
+
+//var object = {"a":{"b":{"c":"d"}}};
+//var key = 'a/b/c' ;
+var object = { x: { y: { z: "a" } } };
+var key = "x/y/z";
+console.log("The value of " + key + " is " + findvalue(object, key));
