6f585d187b7db74bf169f9d3c1251e06c602c87a
RajeshHaridoss <Haridoss.rajesh@gmail.com>
2022-03-19 22:02:38 +0000

    simple vpc set up

---
+++ CodeChallenge1/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+  description = "Project Name"
+  type        = string
+}
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
